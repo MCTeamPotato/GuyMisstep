8461bbc4aa1f5f4ddc56d28f5d66dd9903cc0291
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-08-10 00:43:14 +0800

    1182

--- src/main/java/me/kall/guymisstep/mixin/FallingBlockMixin.java
+++ src/main/java/me/kall/guymisstep/mixin/FallingBlockMixin.java
@@ -6,7 +6,6 @@ import me.kall.guymisstep.network.NetworkManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -15,10 +14,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Random;
+
 @Mixin(FallingBlock.class)
 public abstract class FallingBlockMixin {
     @Inject(method = "animateTick", at = @At("HEAD"))
-    private void onSpawnParticle(BlockState state, Level level, BlockPos pos, RandomSource random, CallbackInfo ci) {
+    private void onSpawnParticle(BlockState state, Level level, BlockPos pos, Random random, CallbackInfo ci) {
         LocalPlayer player = Minecraft.getInstance().player;
         if (player == null) return;
         long blockPos = pos.asLong();
